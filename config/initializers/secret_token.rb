ef55e6cf47a3747fb6e0b8d61737130c1d1baa21
Alex Cobb <cobb.alex@rocketmail.com>
2015-02-07 23:52:58 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cobb::Application.config.secret_key_base = '4b097d22ed127d5ab13d27dba5388609875eab0df1b192a91b1ad8edb1e0cb74155d3bd5f24c329cb0d2711052fe3f0fae84342e4b2381652cfb41f1c38fd1a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cobb::Application.config.session_store :cookie_store, key: '_cobb_session'
